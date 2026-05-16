930e3745872d9cab6f96cd9f81b7a28359090205
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-05-16 23:56:29 +0530

    new.cpp

--- new.cpp
+++ new.cpp
@@ -1,17 +1,1 @@
-#include<iostream>
-#include<climits>
-     
-using namespace std;
- 
-int main()
-{int n=5;
-    int a[n]={1,3,7,4};
-    for(int i=0; i<n; i++){
-        // cout<<*(a+i)<<endl;
-    }
-    char b[5]={'d','f'};
-    cout<<*(b+2);
-
-    return 0;
-
-}
+lll
