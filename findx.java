87d00c3a3de7daabc470db6da143a3221afa1421
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-07-14 17:24:06 +0530

    Update findx.java

--- findx.java
+++ findx.java
@@ -205,20 +205,19 @@ public class Main{
 
 
 // SLiding window problem
+import java.util.HashMap;
+import java.util.Map;
 public class Main{
     public static void main(String args[]){
-        int k=3;
-        int[] arr= {43,6,3,6,32,6,3,-53};
-        int sum=0;
-        for(int i=0; i<k; i++){
-            sum+=arr[i];
-        }
-        int max=sum;
-        for(int i=k; i<arr.length; i++){
-            sum=sum+arr[i]-arr[i-k];
-            max=Math.max(max, sum);
-        }
-        System.out.print(max);
+        Map<Integer, Inetger> map= new HashMap<>(); //create map
+        map.put(1, 5635563); //inseurt values in map
+        map.put(2, 5668444);
+        System.out.print(map.get(1)); //printint values from map
+        map.put(1, 0875937); //updating value
+        System.out.print(map);
+        System.out.print(map.containsKeya(1));
+        map.remove(2);
+        System.out.print(map.size());     
     }
 }
 
@@ -229,3 +228,13 @@ public class Main{
 
 
 
+
+
+
+
+
+
+
+
+
+    
