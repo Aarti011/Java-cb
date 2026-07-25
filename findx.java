3761dfc7487a8544f9b39867025e13ae497d6606
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-07-25 16:13:35 +0530

    Update findx.java

--- findx.java
+++ findx.java
@@ -43,9 +43,6 @@ public class Main{
 
 
 
-
-
-
 // fibonacci using recursion
 
 public class Main{
@@ -283,4 +280,38 @@ public class Main{
     }  
 }
 
+
+
+
+
+
+
+//  java code for Two sum
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int target = 9;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            int diff = target-arr[i];
+
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+" "+i);
+                return;
+            }
+
+            map.put(arr[i],i);
+        }
+    }
+}
+   
+
+
     
+
+
+
