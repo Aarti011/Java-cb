dc9b0d4ae0ef5d78bc06603ff840f8708643679a
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-08-04 00:51:33 +0530

    Update Revise_dsa.java

--- Revise_dsa.java
+++ Revise_dsa.java
@@ -58,5 +58,47 @@ public class Main{
 
 
 
+// java code for sum of first n numbers
+import java.util.Scanner;
+public class Main{
+  public static int func(int n){
+    if(n==0 || n==1)return n;
+    return func(n-1)+n;    
+  }
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    int ans= func(n);
+    System.out.println(ans);
+  }
+}
 
 
+
+
+
+
+//  java code for fibonacci numbers
+import java.util.Scanner;
+public class Main{
+  public static void func(int n,  char s, char a, char d){
+    if(n==1){
+    System.out.println(s+" source to destination "+d);
+    return;
+    }
+    // moving rest disk to auxiliary position
+    func(n-1, s,d,a);
+    
+    // moving largest disk
+    System.out.println(s+" -> "+d);
+    
+    // moving n-1 to helper
+    func(n-1,a,s,d);
+    return ; 
+  }
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    func(n, 'S', 'A', 'D');
+  }
+}
