537741d02dabf75977fc0798d9d81385c0336607
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-08-06 18:34:10 +0530

    Update Revise_dsa.java

--- Revise_dsa.java
+++ Revise_dsa.java
@@ -175,6 +175,23 @@ public class Main{
 
 
 
+// reverse string using stack
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    String s= sc.nextLine(); 
+    Stack<Character> st= new Stack<>();
+    int n= s.length();
+    for(int i=n-1; i>=0; i--){
+        st.push(s.charAt(i));
+    }
+    System.out.print(st);
+  }
+}
+
+
+
 
 
 
