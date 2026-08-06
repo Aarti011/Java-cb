9ef4fbf32ec52f13cd3fb050ec09286f286c4fd3
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-08-06 18:12:50 +0530

    Update Revise_dsa.java

--- Revise_dsa.java
+++ Revise_dsa.java
@@ -102,3 +102,79 @@ public class Main{
     func(n, 'S', 'A', 'D');
   }
 }
+
+
+
+
+
+
+// java code for fibonacci
+import java.util.Scanner;
+public class Main{
+  public static int func(int n){
+    if(n==0 || n==1){return n;}
+    return func(n-1)+func(n-2);
+  }
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt(); 
+    int ans= func(n);
+    System.out.println(ans);
+  }
+}
+
+
+
+
+
+
+//  java code for Stack
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt(); 
+    Stack<Integer> st= new Stack<>();
+    for(int i=0; i<n; i++){
+        st.push(sc.nextInt());
+    }
+    // print complete stack
+    System.out.println(st);
+    // top element i.e at last index
+    System.out.println(st.peek());
+    // check if stack is empty
+    System.out.println(st.isEmpty());
+    // removing element at index
+    st.remove(1);
+    System.out.println(st);
+    // check size of stack
+    System.out.println(st.size());
+    // updating any value at particular index
+    st.set(0, 76);
+    System.out.println(st);
+    // displaying element at index
+    System.out.println(st.get(1));
+    // removing top element of stack
+    System.out.println(st.pop());
+    // remove element with same value
+    st.remove(Integer.valueOf(30));
+    // searching the element in stack
+    System.out.println(st.search(30));
+    // deleting all elements from stack
+    st.clear();
+    System.out.println(st);
+    
+    
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
